8c463c994afbc781ca472a640ed1de959d1b3e20
arkarminn01-png <arkarminn01@gmail.com>
2026-02-10 15:27:13 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import telebot
+
+# Render အတွက် Proxy လုံးဝ မလိုတော့ပါ
+TOKEN = '8225664945:AAFkByoevUyukF8sulcSoiDoFvFCs_Ona3g'
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "🎬 La Yaung Shein Bot အောင်မြင်စွာ အလုပ်လုပ်နေပါပြီ (Render.com)!")
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
 
